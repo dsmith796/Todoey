dbb3ed0d9ffd373cff3b4a3b3dbcd5e3b6f53245
Dannielle Smith <danniellesmith@dannielles-air>
2018-07-18 18:56:16 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dannielle Smith on 18/07/2018.
+//  Copyright © 2018 Dannielle Smith. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+}
